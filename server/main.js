5147a1c280b7af6388eee1984e9df92fedd3a0d7
Yi Lei <lueaon@gmail.com>
2016-07-31 14:45:43 -0500

    because git told me to

--- server/main.js
+++ server/main.js
@@ -4,4 +4,11 @@ import { TimeStuff } from '../api.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.methods({
+    addTime: function(iTimeHr, iTimeMin) {
+      var date = new Date();
+      date.setHours(iTimeHr,iTimeMin,0); //Set seconds to zero
+      console.log("");
+    }
+  })
 });
